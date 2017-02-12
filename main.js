36858803037c20ec79f0176d08e7b9ce3832d576
hanifa arrumaisha <hanifa arrumaisha>
2017-02-12 13:51:15 +0700

    Add Pages

---
+++ main.js
@@ -0,0 +1,33 @@
+function addText(id){
+  if (id == textHobi){
+    if (id.innerHTML == "Click to know more!"){
+      id.innerHTML= "Kini dunia tak dapat dipisahkan dari teknologi, teknologi selalu berkembang dengan pesat. Tak dapat dipungkiri bahwa kita sebagai mahasiswa yang berkecimpung dibidang teknologi pun harus selalu belajar dan belajar agar tidak tertinggal dengan perkembangan teknologi. Kalimat tersebut menjadi alasan mengapa hobi saya adalah belajar, disamping kewajiban saya sebagai mahasiswa, saya yang berminat dibidang teknologi tentu harus terus belajar seiring berkembangnya teknologi, dan belajar yang dimaksudkan juga tidak melulu soal kuliah, tetapi belajar hal baru yang mungkin tak didapatkan saat di kelas.";
+    }else {
+      id.innerHTML= "Click to know more!";
+    }
+  }else if (id == textWeb){
+    if (id.innerHTML == "Click to know more!"){
+      id.innerHTML= "Kenapa sih mau jadi web developer?<br>\
+      Penggunaan web sangat fleksibel, dapat digunakan diberbagai \
+      device tanpa perlu memperhatikan apakah itu laptop/handphone\
+       atau sistem operasinya windows/android/iOS. Selain itu \
+       aplikasi pun ada yang berbasis web, sehingga pengunaan web\
+        dapat diperuntukkan ke segala sektor. <br><br>\
+      Untuk apa jadi web developer?<br>\
+      Web developer banyak dicari oleh perusahaan dan peluangnya\
+       sangat terbuka lebar. Saya ingin memiliki kemampuan tersebut\
+        dan mengaplikasikannya ke ranah yang lebih serius. Bukan \
+        hanya sekadar untuk hobi tetapi juga bisa membawa hasil.<br><br>\
+        Kenapa suka web development?<br>\
+        Jawabannya sudah terdapat di alasan kenapa saya ingin menjadi web developer dan untuk apa menjadi web developer, semua hal itu membuat saya menyukai web development, terlebih saya tidak merasa bosan ketika menjelajah tentang web development."
+    }else {
+      id.innerHTML= "Click to know more!";
+    }
+  }else if (id == textRistek){
+    if (id.innerHTML == "Click to know more!"){
+      id.innerHTML = "Seperti yang sudah disebutkan di bagian \"What's My Hobby?\" tadi, saya senang belajar dan mempelajari hal baru, apalagi tentang hal yang saya suka. Saya menyukai webdev, oleh karena itu saya memilih web development SIG di RISTEK karena saya ingin belajar lebih dalam tentang web development. Selain itu, sebelumnya saya sudah mulai belajar tentang web development dengan mengikuti Open Class RISTEK Webcraft. Saya juga sudah mempelajari basic dari HTML, Javascript, CSS, Bootstrap, dan juga Bahasa Python. ";
+    }else {
+      id.innerHTML= "Click to know more!";
+    }
+  }
+}
